40612c35eaa4b1aa214b56b7ff322e929f1fdf14
Oscar Flores <oscar.floresl@udem.edu>
2023-12-21 16:02:41 -0600

    challenge #2 date counter v2

--- react-course/04.1-challenge-date-counter/src/App.js
+++ react-course/04.1-challenge-date-counter/src/App.js
@@ -14,9 +14,10 @@ function Counter() {
   const [count, setCount] = useState(0);
   // React rerenders everything whenever the state changes
   // Initialize the date and then add whatever state React remembers
-  const date = new Date("december 20 2023");
+  const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
+  /* Unused after V2
   function handlePlusStep() {
     setStep((s) => s + 1);
   }
@@ -24,6 +25,7 @@ function Counter() {
   function handleMinusStep() {
     if (step > 1) setStep((s) => s - 1);
   }
+*/
 
   function handlePlusCount() {
     setCount((c) => c + step);
@@ -33,14 +35,28 @@ function Counter() {
     setCount((c) => c - step);
   }
 
+  function hanldeReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (
     <div>
-      <button onClick={handleMinusStep}>-</button>
-      Step: {step}
-      <button onClick={handlePlusStep}>+</button>
+      <input
+        type="range"
+        min="1"
+        max="10"
+        value={step}
+        onChange={(e) => setStep(Number(e.target.value))}
+      />
+      <span>{step}</span>
       <br></br>
       <button onClick={handleMinusCount}>-</button>
-      Count: {count}
+      <input
+        type="text"
+        value={count}
+        onChange={(e) => setCount(Number(e.target.value))}
+      ></input>
       <button onClick={handlePlusCount}>+</button>
       <br></br>
       <h1>
@@ -51,6 +67,10 @@ function Counter() {
           : `${Math.abs(count)} days ago was`}{" "}
         {date.toDateString()}
       </h1>
+      {/* Conditionally display reset button if values are not default*/}
+      {count !== 0 || step !== 1 ? (
+        <button onClick={hanldeReset}>Reset</button>
+      ) : null}
     </div>
   );
 }
